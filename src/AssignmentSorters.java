7fb7d65cbfd6a79fb7b1f449220fa35c95607f15
uzbek <ekretov25@gmail.com>
2026-02-25 19:29:14 +0700

    Sorters

---
+++ src/AssignmentSorters.java
@@ -0,0 +1,15 @@
+import java.util.Comparator;
+
+public class AssignmentSorters {
+    public static Comparator<RoleAssignment> byUsername() {
+        return Comparator.comparing(a -> a.user().username());
+    }
+
+    public static Comparator<RoleAssignment> byRoleName() {
+        return Comparator.comparing(a -> a.role().getName());
+    }
+
+    public static Comparator<RoleAssignment> byAssignmentDate() {
+        return Comparator.comparing(a -> a.metadata().assignedAt());
+    }
+}
---
+++ src/RoleSorters.java
@@ -0,0 +1,12 @@
+import java.util.Comparator;
+
+public class RoleSorters {
+    public static Comparator<Role> byName(){
+        return Comparator.comparing(Role::getName);
+    }
+
+    public static Comparator<Role> byPermissionCount(){
+        return Comparator.comparingInt(o -> o.getPermissions().size());
+    }
+
+}
